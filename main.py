85200c0d9e559efa453f159e10d45897c53791ba
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-26 23:27:37 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-# Import matplotlib library here
 import matplotlib.pyplot as plt
 
-# Let's rank some of our favorite snacks
-snack_scores = [100, 80, 60]
-snack_names = ["yogurt", "watermelon", "peanuts"]
-               
-plt.pie(snack_scores, labels=snack_names)
+snack_scores = [80,45,34]
 
-# Give your pie chart a title in the quotes
-plt.title("Thais' favorite snacks")
+slice_labels = ["Chocolate", "Cheese", "Pickles"]
 
-# Put the name of your file in the quotes and give it a .png extension
-plt.savefig("SnackVisual.png")
+colors = ["#5F9EA0","#7FFFD4","#FF69B4"]
+
+plt.pie(snack_scores, labels=slice_labels, colors=colors, )
+
+plt.title("Snack Scores",  fontsize=16)
+
+plt.savefig("snack_scores.png")
