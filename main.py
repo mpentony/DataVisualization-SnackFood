a6c26335ab40280fdad9329989648ec9a7d6c4ba
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-27 09:38:03 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
-# Import matplotlib library here
 import matplotlib.pyplot as plt
 
-# Let's rank some of our favorite snacks
-snack_scores = [100, 80, 60]
-snack_names = ["yogurt", "watermelon", "peanuts"]
-               
-plt.pie(snack_scores, labels=snack_names)
+#Data
+month = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"]
+temp = [28,32,31,40,45,55,60,65,54,43,34,30]
 
-# Give your pie chart a title in the quotes
-plt.title("Thais' favorite snacks")
+#Plotting
+plot = plt.plot(month, temp, color="purple")
 
-# Put the name of your file in the quotes and give it a .png extension
-plt.savefig("SnackVisual.png")
+#Labeling
+plt.xlabel("Month", fontsize=16)
+plt.ylabel("Temperature in Fahrenheit", fontsize=16)
+plt.title("Avg Temperatures for 2018 in North Pole, Alaska")
+
+#Saving to a file
+plt.savefig("lineplot.png")
