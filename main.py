f468f88be3d2fee27c2033a0a409b99dbd7d9a60
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-27 09:57:36 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-# Import matplotlib library here
 import matplotlib.pyplot as plt
 
-# Let's rank some of our favorite snacks
-snack_scores = [100, 80, 60]
-snack_names = ["yogurt", "watermelon", "peanuts"]
-               
-plt.pie(snack_scores, labels=snack_names)
+#Data
+movie_year = [2019,2019,2018,2018,2017,2016,2016,2016,2015,2015,2014,2014,2014,2013,2012,2012,2011,2010,2010,2009,2009,2008,2008,2007,2007,2007,2007,2006,2006,2006,2006,2005,2004,2004,2003,2003,2003,2002,2002,2001,1999,1999,1998,1998,1997,1996,1996,1995,1994,1993,1992,1991,1990,1989,1988,1987,1986,1985,1985,1984,1983,1982,1982,1982,1981,1979,1979,1979,1978,1978,1977,1977]
 
-# Give your pie chart a title in the quotes
-plt.title("Thais' favorite snacks")
+bins =[1976,1985, 1986,1997, 1998,2007, 2008,2019]
 
-# Put the name of your file in the quotes and give it a .png extension
-plt.savefig("SnackVisual.png")
+#plot
+plt.hist(movie_year, bins, histtype='bar')
+
+#Labeling
+plt.xlabel('Year')
+plt.ylabel('Number of Movies')
+plt.title("Meryl Streep's Movies Career")
+
+plt.savefig("historgram1.png")
+
