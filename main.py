6227e63fd8bb2f34182b70ed8f4cdff8c26afa34
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-07-29 08:11:57 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
-# Import matplotlib library here
-import matplotlib.pyplot as plt
+import csv
+import numpy as np
 
-# Let's rank some of our favorite snacks
-snack_scores = [100, 80, 60]
-snack_names = ["yogurt", "watermelon", "peanuts"]
-               
-plt.pie(snack_scores, labels=snack_names)
+with open("tips.csv", "r") as file:
+  data = csv.reader(file,delimiter=",")
+  headers = next(data)
+  data_list = list(data)
+  data_numpy = np.array(data_list)
 
-# Give your pie chart a title in the quotes
-plt.title("Thais' favorite snacks")
+size=data_numpy[:6]
+#print(size)
 
-# Put the name of your file in the quotes and give it a .png extension
-plt.savefig("SnackVisual.png")
+tips=np.array(data_numpy[:,1],dtype=float)
+#print(tips)
+
+bills=np.array(data_numpy[:,0],dtype=float)
+#print(bill)
+
+tips_percentage=(tips/bills)*100
+#print(tips_percentage)
+
+#print average mean of the bills array
+print(f"The average bill amount is ${round(np.mean(bills),2)}")
+
+#print the median of the bills array
+print(f"The median bill amount is ${round(np.median(bills),2)}")
+
+#print the smallest and maximum bill amount
+print(f"The smallest bill amount is ${round(np.min(bills),2)}")
+print(f"The largest bill amount is ${round(np.max(bills),2)}")
+
+
