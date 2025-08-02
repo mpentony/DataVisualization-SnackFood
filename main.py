d8372c10e675dfa2e5ea4ab9ae9d3857e56957a9
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-08-02 10:56:55 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,48 @@
-# Import matplotlib library here
-import matplotlib.pyplot as plt
+import requests
+from bs4 import BeautifulSoup
 
-# Let's rank some of our favorite snacks
-snack_scores = [100, 80, 60]
-snack_names = ["yogurt", "watermelon", "peanuts"]
-               
-plt.pie(snack_scores, labels=snack_names)
 
-# Give your pie chart a title in the quotes
-plt.title("Thais' favorite snacks")
+def get_soup(url):
+  r = requests.get(url)
+  r.raise_for_status()
+  html = r.text.encode("utf-8")
+  soup = BeautifulSoup(html, "html.parser")
+  return soup
 
-# Put the name of your file in the quotes and give it a .png extension
-plt.savefig("SnackVisual.png")
+def get_categories(url):
+  soup = get_soup(url)
+  data = {}
+  categories = soup.find_all("dl")
+  for category in categories:
+    category_name = category.find("dt").get_text()
+    category_animals = category.find_all("a")
+    data[category_name] = category_animals
+  return data
+
+def get_animal(url):
+  soup = get_soup(url)
+  table = soup.find("table", {"class": "infobox biota"})
+  if not table:
+    return "No class found."
+  rows = table.find_all("tr")
+  for row in rows:
+    if "Class:" in row.get_text():
+      animal_class = row.find("a").contents[0]
+  return animal_class
+
+
+category_data = get_categories("https://skillcrush.github.io/web-scraping-endangered-species/")
+
+# print(category_data)
+
+# animal_class = get_animal("https://en.wikipedia.org/wiki/Honey_badger")
+
+# print(animal_class)
+
+for category in category_data:
+  for animal in category_data[category]:
+    animal_href = animal["href"]
+    # print(animal_href)
+    animal_class = get_animal(animal_href)
+    print(animal_class)
+    print()
