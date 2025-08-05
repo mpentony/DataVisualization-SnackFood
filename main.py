64bf98c2e4e75d9779ce99b8660e6e1dd11bb935
Monique Pentony <97858129+mpentony@users.noreply.github.com>
2025-08-05 08:42:34 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-# Import matplotlib library here
 import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
 
-# Let's rank some of our favorite snacks
-snack_scores = [100, 80, 60]
-snack_names = ["yogurt", "watermelon", "peanuts"]
-               
-plt.pie(snack_scores, labels=snack_names)
+with open("tips.csv", "r",) as csvfile:
+  tips=pd.read_csv(csvfile, delimiter=",")
 
-# Give your pie chart a title in the quotes
-plt.title("Thais' favorite snacks")
+tips_pivoted=tips.pivot_table(values="tip", index="size", columns="time")
 
-# Put the name of your file in the quotes and give it a .png extension
-plt.savefig("SnackVisual.png")
+fig=sns.heatmap(tips_pivoted, annot=True, cmap="Spectral")
+fig.set_ylim(6,0)
+
+plt.xlabel("Time of day")
+plt.ylabel("Group size")
+plt.title("Tip amount by group size and time of day")
+plt.savefig("heatmap.png")
+
